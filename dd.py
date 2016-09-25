a9a66a3b82a4528e4fc9f1a4a3125857ca821270
varun_s <varunsudharshan@gmail.com>
2016-09-25 21:25:21 +0530

    original code

---
+++ dd.py
@@ -0,0 +1,36 @@
+import msvcrt
+import time
+def toprow():
+	finish=10
+	count=0
+	while(1):
+		key=ord(msvcrt.getch())
+		if key== 224:
+			key=ord(msvcrt.getch())
+			if key== 77:
+				count=count+1
+				print "-->",
+			if count==finish:
+				break
+def midrow():
+	finish=10
+	count=0
+	while(1):
+		key=ord(msvcrt.getch())
+		if key== 224:
+			key=ord(msvcrt.getch())
+			if key== 80:
+				count=count+1
+				print "\n                                       |",
+			if count==finish:
+				break
+print "press enter key to get started!"
+raw_input()
+s_time=time.time()
+toprow()
+midrow()
+print "\n                                      ",
+toprow()
+time_elapsed=time.time()-s_time
+print "\ncongrats you have finished the game"
+print "time taken is "+str(time_elapsed)
